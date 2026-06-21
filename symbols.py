86f0420c39328cc39325a94249df1c70468244ac
asrarabha8-cmyk <asrarabha8@gmail.com>
2026-06-21 13:15:13 +0300

    Create symbols.py

---
+++ symbols.py
@@ -0,0 +1,64 @@
+SYMBOLS = [
+
+AI / Software
+
+“NVDA”,“AMD”,“AVGO”,“ARM”,“PLTR”,“CRWD”,“SNOW”,“DDOG”,
+“NET”,“MDB”,“ZS”,“PANW”,“NOW”,“ADBE”,“CRM”,“MSFT”,
+“GOOGL”,“AMZN”,“META”,“ORCL”,“PATH”,“U”,“DUOL”,
+
+Semiconductors
+
+“MU”,“MRVL”,“AMAT”,“LRCX”,“KLAC”,“TSM”,“QCOM”,
+“ASML”,“SMCI”,“CRDO”,“MCHP”,“ON”,“ADI”,“NXPI”,
+
+Space / Defense
+
+“ASTS”,“RKLB”,“LUNR”,“PL”,“RDW”,“ACHR”,“JOBY”,
+“KTOS”,“AVAV”,“LHX”,“NOC”,“RTX”,“LMT”,
+
+Crypto
+
+“MSTR”,“COIN”,“MARA”,“RIOT”,“CLSK”,“CIFR”,“BTDR”,
+
+Growth
+
+“HIMS”,“SOFI”,“HOOD”,“UPST”,“AFRM”,“RIVN”,“LCID”,
+“CELH”,“CAVA”,“DASH”,“ABNB”,“RBLX”,
+
+Biotech / AI Health
+
+“TEM”,“RXRX”,“EXAS”,“ILMN”,“NTRA”,“BEAM”,“CRSP”,
+
+Internet / Consumer
+
+“SHOP”,“MELI”,“SE”,“PINS”,“ROKU”,“SPOT”,
+
+Energy / Uranium
+
+“CCJ”,“UUUU”,“SMR”,“OKLO”,“NNE”,“LEU”,
+
+Quantum
+
+“IONQ”,“RGTI”,“QBTS”,“QUBT”,
+
+Big Tech
+
+“AAPL”,“TSLA”,“NFLX”,“INTC”,“IBM”,“UBER”,
+
+Financial Tech
+
+“PYPL”,“SQ”,“COF”,“AXP”,“V”,“MA”,
+
+Cloud
+
+“DOCN”,“ESTC”,“FSLY”,“CFLT”,“S”,“GTLB”,
+
+Cybersecurity
+
+“FTNT”,“CYBR”,“SENT”,“OKTA”,
+
+Small Cap Growth
+
+“APLD”,“BBAI”,“SOUN”,“LAES”,“ENVX”,“QS”,
+“DNA”,“ARQQ”,“IREN”,“NBIS”
+]
